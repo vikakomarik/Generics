43883f79c862aaaf5d5da90336567827fe9811dd
Viktoriia Komarova <vikamail2011@gmail.com>
2023-03-31 15:11:18 +0200

    Використання параметру collection

--- src/main/java/org/example/Box.java
+++ src/main/java/org/example/Box.java
@@ -9,9 +9,9 @@ import java.util.Collection;
 public class Box<T, U, V> {
 
     //Всередині цього класу створити змінні цих типів та гетери до них.
-    private T boxT;
-    private U boxU;
-    private V boxV;
+    private final T boxT;
+    private final U boxU;
+    private final V boxV;
 
     public T getT() {
         return boxT;
@@ -35,10 +35,11 @@ public class Box<T, U, V> {
 //Створити метод, який буде приймати, як параметр, колекцію з довільними типами
 // обʼєктів та виводити на екран назву кожного типу.
     public void printTypes(Collection<?> collection) {
-        System.out.println("boxT: " + boxT.getClass().getSimpleName());
-        System.out.println("boxU: " + boxU.getClass().getSimpleName());
-        System.out.println("boxV: " + boxV.getClass().getSimpleName());
+            for (Object item : collection) {
+                System.out.println(item.getClass().getSimpleName());
+            }
+        }
 
     }
-}
+
 
